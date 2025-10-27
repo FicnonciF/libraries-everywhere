b07f18219f6d8ec4bb8ce34f268eb6e1c04d6990
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-10-27 19:42:37 +0530

    Added Carousel

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom'
 const Header: React.FC = () => {
 	return (
 		<>
-			<header className='px-24 py-4 flex justify-between items-center fixed top-0 left-0 right-0 z-50'>
+			<header className='px-24 pt-10 flex justify-between items-center fixed top-0 left-0 right-0 z-50'>
 				<Link to="/">
-					<img src={logo} alt="" width={50} className='invert' />
+					<img src={logo} alt="" width={80} className='invert' />
 				</Link>
 
 				<nav className='flex gap-4 text-lg font-semibold'>
-					<Link to="/join">Join</Link>
-					{/* <Link to="/contact">Contact</Link>
-                    <Link to="/profile">Profile</Link> */}
+					<Link to="/join"
+						className='w-20 h-5 bg-white rounded-full flex items-center justify-center p-4.5 font-semibold'>
+						Join
+					</Link>
 				</nav>
 			</header>
 		</>
